478457631b99c8ce191a7ef080d73b0e977d39ca
gautamkumar30 <gautamkumarganesan@gmail.com>
2024-04-02 11:49:29 +0530

    minor changes in submitted papers page

--- client/src/pages/SubmittedPapers.jsx
+++ client/src/pages/SubmittedPapers.jsx
@@ -1,20 +1,20 @@
 const PaperCard = () => {
   return (
     <div className="flex flex-row justify-between">
-      <div className="flex flex-row gap-7">
+      <div className="flex flex-row gap-7 justify-center items-center">
         <div className="w-[70px] h-[70px] bg-secondary rounded-xl"></div>
         <div>
-          <p className="text-primary font-semibold text-[20px]">
-            Paper on Green Energy Sustainability{" "}
+          <p className="text-primary font-semibold text-[18px] tracking-tight">
+            Paper on Green Energy Sustainability
           </p>
-          <p className="text-primary font-medium text-[18px] mt-1">
+          <p className="text-primary font-medium text-[18px] mt-1 tracking-tight opacity-80">
             John Mishra
           </p>
         </div>
       </div>
-      <div className="flex flex-col gap-0 items-end">
-        <p className="text-[30px] font-semibold text-primary leading-0">4.5</p>
-        <button className="text-secondary font-semibold">RATE NOW</button>
+      <div className="flex flex-col gap-0 items-end justify-center">
+        <p className="text-[25px] font-semibold text-primary leading-0">4.5</p>
+        <button className="text-secondary text-[16px] font-semibold">RATE NOW</button>
       </div>
     </div>
   );
