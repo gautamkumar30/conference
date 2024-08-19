52cff256f278caef1060b2af4a3dc816656929e0
gautamkumar30 <137641850+gautamkumar30@users.noreply.github.com>
2024-08-19 22:02:32 +0530

    Update UserContext.jsx

--- client/src/contexts/UserContext.jsx
+++ client/src/contexts/UserContext.jsx
@@ -6,7 +6,7 @@ export const UserContext = createContext({});
 export function UserContextProvider({ children }) {
   const [userInfo, setUserInfo] = useState({});
   return (
-    <UserContext.Provider value={{ userInfo, setUserInfo }}>
+    <UserContext.Provider value={{ userInfo, setUserInfo }}>  
       {children}
     </UserContext.Provider>
   );
